6feb5ffb2b9733514720753b9d90a21333308f1c
Martin Geisler <mgeisler@google.com>
2022-05-18 15:47:43 +0200

    pdl: correctly apply mask in ‘write_to’ as well
    Before we only applied a mask in ‘parse’, and we always used a
    hard-coded mask of the wrong size.
    
    Test: atest pdl_inline_tests
    Change-Id: Id499e009d2d83e9eb5e4aaffc375e527a1995864

--- src/generator.rs
+++ src/generator.rs
@@ -122,6 +122,26 @@ fn generate_field_getter(packet_name: &syn::Ident, field: &ast::Field) -> proc_m
     }
 }
 
+/// Mask and rebind the field value (if necessary).
+fn mask_field_value(field: &ast::Field) -> Option<proc_macro2::TokenStream> {
+    match field {
+        ast::Field::Scalar { id, width, .. } => {
+            let field_name = format_ident!("{id}");
+            let type_width = round_bit_width(*width);
+            if *width != type_width {
+                let mask =
+                    syn::parse_str::<syn::LitInt>(&format!("{:#x}", (1u64 << *width) - 1)).unwrap();
+                Some(quote! {
+                    let #field_name = #field_name & #mask;
+                })
+            } else {
+                None
+            }
+        }
+        _ => todo!("unsupported field: {:?}", field),
+    }
+}
+
 fn generate_field_parser(
     endianness_value: &ast::EndiannessValue,
     packet_name: &str,
@@ -150,13 +170,7 @@ fn generate_field_parser(
 
             let wanted_len = syn::Index::from(offset + width / 8);
             let indices = (offset..offset + width / 8).map(syn::Index::from);
-            let mask = if *width != type_width {
-                Some(quote! {
-                    let #field_name = #field_name & 0xfff;
-                })
-            } else {
-                None
-            };
+            let mask = mask_field_value(field);
 
             quote! {
                 // TODO(mgeisler): call a function instead to avoid
@@ -187,16 +201,17 @@ fn generate_field_writer(
     match field {
         ast::Field::Scalar { id, width, .. } => {
             let field_name = format_ident!("{id}");
-            let bit_width = round_bit_width(*width);
             let start = syn::Index::from(offset);
-            let end = syn::Index::from(offset + bit_width / 8);
-            let byte_width = syn::Index::from(bit_width / 8);
+            let end = syn::Index::from(offset + width / 8);
+            let byte_width = syn::Index::from(width / 8);
+            let mask = mask_field_value(field);
             let writer = match file.endianness.value {
                 ast::EndiannessValue::BigEndian => format_ident!("to_be_bytes"),
                 ast::EndiannessValue::LittleEndian => format_ident!("to_le_bytes"),
             };
             quote! {
                 let #field_name = self.#field_name;
+                #mask
                 buffer[#start..#end].copy_from_slice(&#field_name.#writer()[0..#byte_width]);
             }
         }
@@ -535,6 +550,7 @@ mod tests {
               packet Foo {
                 x: 8,
                 y: 16,
+                z: 24,
               }
             "#,
         );
@@ -557,6 +573,7 @@ mod tests {
               packet Foo {
                 x: 8,
                 y: 16,
+                z: 24,
               }
             "#,
         );
@@ -589,6 +606,16 @@ mod tests {
             "right",
             &rustfmt(&right.to_string()),
         );
+    }
+
+    #[test]
+    fn test_mask_field_value() {
+        let loc = ast::SourceRange::default();
+        let field = ast::Field::Scalar { loc: loc.clone(), id: String::from("a"), width: 8 };
+        assert_eq!(mask_field_value(&field).map(|m| m.to_string()), None);
+
+        let field = ast::Field::Scalar { loc, id: String::from("a"), width: 24 };
+        assert_expr_eq(mask_field_value(&field).unwrap(), quote! { let a = a & 0xffffff; });
     }
 
     #[test]
@@ -629,7 +656,7 @@ mod tests {
                     });
                 }
                 let a = u32::from_le_bytes([bytes[10], bytes[11], bytes[12], 0]);
-                let a = a & 0xfff;
+                let a = a & 0xffffff;
             },
         );
     }
@@ -651,7 +678,7 @@ mod tests {
                     });
                 }
                 let a = u32::from_be_bytes([0, bytes[10], bytes[11], bytes[12]]);
-                let a = a & 0xfff;
+                let a = a & 0xffffff;
             },
         );
     }
