a3147830df71d4c4815b66438041fb692087fb12
uael <uael@google.com>
2023-07-06 08:05:54 -0700

    Fix some typos

--- src/backends/json.rs
+++ src/backends/json.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Rust compiler backend.
+//! Json compiler backend.
 
 use crate::parser;
 
