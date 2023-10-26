620cbf2f0e886e3ddd7489d465baa6f52e4dd8c2
Henri Chataing <henrichataing@google.com>
2023-10-26 10:06:09 -0700

    Move back Private<T> definition to preamble
    - Generated module needs private access to the
      Private struct..
    - TODO update workflow to compile generated
      test files

--- pdl-compiler/tests/generated/packet_decl_empty_little_endian.rs
+++ pdl-compiler/tests/generated/packet_decl_empty_little_endian.rs
@@ -4,8 +4,20 @@ use bytes::{Buf, BufMut, Bytes, BytesMut};
 use std::convert::{TryFrom, TryInto};
 use std::cell::Cell;
 use std::fmt;
-use pdl_runtime::{Error, Packet, Private};
+use pdl_runtime::{Error, Packet};
 type Result<T> = std::result::Result<T, Error>;
+/// Private prevents users from creating arbitrary scalar values
+/// in situations where the value needs to be validated.
+/// Users can freely deref the value, but only the backend
+/// may create it.
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Private<T>(T);
+impl<T> std::ops::Deref for Private<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct FooData {}
